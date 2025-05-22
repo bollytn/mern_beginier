da5a348f6ac22f8d475124b78c9d3ebc775dced0
unknown <mahjoubi.bilel@gmail.com>
2025-05-22 02:05:44 +0100

    update tsconfig

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Axios from 'axios'
 
+
 /*
 const users = [
     {
@@ -19,6 +20,7 @@ const Cards = () => {
         Axios.get('http://localhost:3001/users')
             .then((response) => {
                 setUsers(response.data)
+                console.log(response.data)
             })
             .catch((error) => {
                 console.error("There was an error fetching the users!", error);
@@ -27,17 +29,21 @@ const Cards = () => {
 
 
     return (
-        <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {users.map((user, index) => {
-                return (
-                    <div key={index} className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow">
-                        <h1 className="text-xl font-bold text-gray-800">{user.name}</h1>
-                        <p className="text-gray-600 mt-2">Age: {user.age}</p>
-                        <p className="text-gray-600 mt-1">{user.email}</p>
-                    </div>
-                )
-            })}
-        </div>
+        <>
+            <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {users.map((user, index) => {
+                    return (
+                        <div key={index} className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow">
+                            <h1 className="text-xl font-bold text-gray-800">{user.name}</h1>
+                            <p className="text-gray-600 mt-2">Age: {user.age}</p>
+                            <p className="text-gray-600 mt-1">{user.email}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
+
+        </>
     )
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths"
+import { fileURLToPath,} from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/components'),
+    },
+  },
 })
